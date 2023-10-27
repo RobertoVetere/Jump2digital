743aa3d18fa3212813168d2e48d17b964c8fb62f
Roberto Vetere <Roberto.vetere@gmail.com>
2023-10-27 18:53:37 +0200

    ADD SkinRepository tests

--- src/main/java/com/roberv/skin/repository/SkinRepository.java
+++ src/main/java/com/roberv/skin/repository/SkinRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 @Repository
 public interface SkinRepository extends JpaRepository<Skin, Long> {
 
+    Skin findByName(String testSkin);
+
+    List<Skin> findByType(String type1);
 }
--- src/test/java/com/roberv/skin/repositoryTests/SkinRepositoryTests.java
+++ src/test/java/com/roberv/skin/repositoryTests/SkinRepositoryTests.java
@@ -1,13 +1,57 @@
 package com.roberv.skin.repositoryTests;
 
+import com.roberv.skin.models.Skin;
+import com.roberv.skin.repository.SkinRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SkinRepositoryTests {
+import java.util.List;
 
-	@Test
-	void contextLoads() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class SkinRepositoryTests {
+
+	@Autowired
+	private SkinRepository skinRepository;
+
+	@BeforeEach
+	public void setUp() {
+
+		Skin existingSkin = skinRepository.findByName("TestSkin");
+		if (existingSkin == null) {
+			Skin skin = new Skin("TestSkin", "Type", 10.99, "Red", "Description");
+			skinRepository.save(skin);
+		}
 	}
 
+	@Test
+	public void testFindByName() {
+		Skin foundSkin = skinRepository.findByName("TestSkin");
+
+		assertNotNull(foundSkin);
+		assertEquals("TestSkin", foundSkin.getName());
+	}
+
+	@Test
+	public void testFindByType() {
+		Skin skin1 = new Skin("Skin1", "Type1", 10.99, "Red", "Description");
+		Skin skin2 = new Skin("Skin2", "Type2", 15.99, "Blue", "Description");
+		skinRepository.saveAll(List.of(skin1, skin2));
+
+		List<Skin> skins = skinRepository.findByType("Type1");
+
+		assertEquals(1, skins.size());
+		assertEquals("Skin1", skins.get(0).getName());
+	}
+
+	@AfterEach
+	public void tearDown() {
+		skinRepository.deleteAll();
+	}
 }
