a7b6412c1bcf0103bb0788264b8138b21907274d
Roberto Vetere <Roberto.vetere@gmail.com>
2023-10-27 13:49:17 +0200

    first commit

---
+++ src/main/java/com/roberv/skin/SkinApplication.java
@@ -0,0 +1,13 @@
+package com.roberv.skin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberv/skin/SkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberv.skin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
