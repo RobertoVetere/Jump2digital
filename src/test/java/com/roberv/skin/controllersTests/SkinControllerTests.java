348eea61694cbea7adc0e2179670e2c738a0b980
Roberto Vetere <Roberto.vetere@gmail.com>
2023-10-27 18:45:42 +0200

    ADD Model tests and controllers tests

--- src/test/java/com/roberv/skin/controllersTests/SkinControllerTests.java
+++ src/test/java/com/roberv/skin/controllersTests/SkinControllerTests.java
@@ -1,13 +1,52 @@
 package com.roberv.skin.controllersTests;
 
+import com.roberv.skin.controller.SkinController;
+import com.roberv.skin.service.SkinService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SkinControllerTests {
 
-	@Test
-	void contextLoads() {
+	@InjectMocks
+	private SkinController skinController;
+
+	@Mock
+	private SkinService skinService;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
 	}
 
+	@Test
+	public void testGetAvailableSkins() {
+		// Mock del servicio para devolver una respuesta simulada
+		String mockResponse = "Lista de skins disponibles";
+		when(skinService.getAvaiableSkins()).thenReturn(ResponseEntity.ok(mockResponse));
+
+		// Llama al controlador y verifica la respuesta
+		ResponseEntity<String> response = skinController.getAvailableSkins();
+		assertEquals(mockResponse, response.getBody());
+	}
+
+	@Test
+	public void testBuySkin() {
+		// Mock del servicio para devolver una respuesta simulada
+		String mockResponse = "Skin comprada con éxito.";
+		when(skinService.buySkin(anyString())).thenReturn(ResponseEntity.ok(mockResponse));
+
+		// Llama al controlador y verifica la respuesta
+		ResponseEntity<String> response = skinController.buySkin("SkinName");
+		assertEquals(mockResponse, response.getBody());
+	}
 }
--- src/test/java/com/roberv/skin/modelsTests/SkinModelTests.java
+++ src/test/java/com/roberv/skin/modelsTests/SkinModelTests.java
@@ -1,13 +1,55 @@
 package com.roberv.skin.modelsTests;
 
+import com.roberv.skin.models.Skin;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SkinModelTests {
 
+	private Skin skin;
+
+	@BeforeEach
+	public void setUp() {
+		skin = new Skin("Skin 1", "Type 1", 10.99, "Red", "Description for Skin 1");
+	}
+
 	@Test
-	void contextLoads() {
+	public void testSkinInitialization() {
+		assertNotNull(skin);
+	}
+
+	@Test
+	public void testSkinName() {
+		assertEquals("Skin 1", skin.getName());
+	}
+
+	@Test
+	public void testSkinType() {
+		assertEquals("Type 1", skin.getType());
+	}
+
+	@Test
+	public void testSkinPrice() {
+		assertEquals(10.99, skin.getPrice(), 0.01);
+	}
+
+	@Test
+	public void testSkinColor() {
+		assertEquals("Red", skin.getColor());
+	}
+
+	@Test
+	public void testSkinDescription() {
+		assertEquals("Description for Skin 1", skin.getDescription());
+	}
+
+	@Test
+	public void testSkinIdIsNullInitially() {
+		assertNull(skin.getId());
 	}
 
 }
