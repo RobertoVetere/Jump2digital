bec57d480744b024ac58b48724e3df03cb2d4ef6
Roberto Vetere <Roberto.vetere@gmail.com>
2023-10-27 18:40:21 +0200

    ADD TDD tests classes

---
+++ src/test/java/com/roberv/skin/modelsTests/SkinModelTests.java
@@ -0,0 +1,13 @@
+package com.roberv.skin.modelsTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinModelTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/roberv/skin/serviceTests/SkinServiceTests.java
@@ -0,0 +1,13 @@
+package com.roberv.skin.serviceTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
